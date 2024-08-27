904ff60886745e52a7d65f3ecb66b3188a4014c3
Mustafa Gedikli <mustafa.gedikli@nttdata.com>
2024-08-27 15:24:07 +0000

    Beginnin' of App Vertical

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,13 +5,16 @@
 sap.ui.define([
         "sap/ui/core/UIComponent",
         "sap/ui/Device",
-        "vertical/project123/model/models"
+        "vertical/project123/model/models",
+        "sap/ui/model/json/JSONModel",
+        "sap/ui/model/resource/ResourceModel",
     ],
-    function (UIComponent, Device, models) {
+    function (UIComponent, Device, models, JSONModel, ResourceModel,) {
         "use strict";
 
         return UIComponent.extend("vertical.project123.Component", {
             metadata: {
+                interfaces: ["sap.ui.core.IAsyncContentCreation"],
                 manifest: "json"
             },
 
@@ -29,6 +32,22 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+
+                 // set data model on view
+                const oData = {
+                recipient : {
+                name : "Rabbit"
+                    }
+                };
+
+                const oModel = new JSONModel(oData);
+                this/* .getView() */.setModel(oModel);
+
+                //setting i18n_jp model here
+                const i18nModel = new ResourceModel({
+                    bundleName: "vertical.project123.i18n.i18n_jp"
+                });
+                this/* .getView() */.setModel(i18nModel, "i18n_jp");
             }
         });
     }
--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
 ],
-function (Controller) {
+function (Controller, MessageToast, JSONModel, ResourceModel,) {
     "use strict";
 
     return Controller.extend("vertical.project123.controller.View1", {
         onInit: function () {
+           
+        },
+        
+        
+        // my mouse click event listener function
+        onClick() {
+            //reading message from i18Model
+            const oBundle = this.getView().getModel("i18n_jp").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
 
-        }
-    });
+            // and the messege that is going to show up
+            MessageToast.show(sMsg);
+            },
+        
+}); 
+
 });
